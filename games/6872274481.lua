096f55e56e6e5de9f57d044679a4ab5910eb76a1
skidrewrite <andyisgreasy@gmail.com>
2026-07-18 14:58:33 -0700

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -34586,9 +34586,9 @@ run(function()
         if not delta then return false end
         local currentTime = tick()
         local delayBetweenAttacks
-        if delta > 20 then delayBetweenAttacks = 0.26
-        elseif delta > 18 then delayBetweenAttacks = 0.23
-        elseif delta > 14.4 then delayBetweenAttacks = 0.17
+        if delta > 20 then delayBetweenAttacks = 0.28
+        elseif delta > 18 then delayBetweenAttacks = 0.25
+        elseif delta > 14.4 then delayBetweenAttacks = 0.18
         elseif delta > 10 then delayBetweenAttacks = 0.04
         else delayBetweenAttacks = 0 end
         local elapsed = currentTime - lastOptimizedAttackTime
