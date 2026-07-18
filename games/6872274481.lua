5589c228405bb4d6e69455d2b370032bf46f9f24
skidrewrite <andyisgreasy@gmail.com>
2026-07-18 12:55:16 -0700

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -34586,10 +34586,10 @@ run(function()
         if not delta then return false end
         local currentTime = tick()
         local delayBetweenAttacks
-        if delta > 20 then delayBetweenAttacks = 0.38
-        elseif delta > 18 then delayBetweenAttacks = 0.36
-        elseif delta > 14.4 then delayBetweenAttacks = 0.25
-        elseif delta > 10 then delayBetweenAttacks = 0.1
+        if delta > 20 then delayBetweenAttacks = 0.21
+        elseif delta > 18 then delayBetweenAttacks = 0.21
+        elseif delta > 14.4 then delayBetweenAttacks = 0.21
+        elseif delta > 10 then delayBetweenAttacks = 0.01
         else delayBetweenAttacks = 0 end
         local elapsed = currentTime - lastOptimizedAttackTime
         if elapsed >= delayBetweenAttacks then
@@ -34625,7 +34625,7 @@ local function FireAttackRemote(attackTable)
     if not canHitWithCustomReg() then return end
 
     local now = workspace:GetServerTimeNow()
-    if now - lastSuccessfulHitTime < 0 then return end   -- spam protection
+    if now - lastSuccessfulHitTime < 6000 then return end   -- spam protection
 
     local _atkPlr = playersService:GetPlayerFromCharacter(attackTable.entityInstance)
     if _atkPlr then
